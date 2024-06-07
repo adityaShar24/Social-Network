9eecfa1eebe0c5ae5aa6f7ae00e8e87415c2bbc2
adityaShar24 <adityarajansharma1234@gmail.com>
2024-06-07 21:14:01 +0530

    Initial Setup

---
+++ SocialNetwork/UserAuthentication/models.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email field must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        
+        return self.create_user(email, username, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(_('username'), max_length=150, unique=True, error_messages={'unique': _("A user with that username already exists.")})
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+        error_messages={'unique': _("A user with that email already exists.")},
+    )
+    password = models.CharField(_('password'), max_length=128)
+    
+    is_active = models.BooleanField(_('active'), default=True)
+    is_staff = models.BooleanField(_('staff status'), default=False)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return self.email
+        
+    def save(self, *args, **kwargs):
+        self.email = self.email.lower()
+        super().save(*args, **kwargs) 
+    
