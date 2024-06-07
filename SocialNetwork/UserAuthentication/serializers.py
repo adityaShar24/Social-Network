18590ec2bc1edd28c9973f1312bce8fe892a8c8c
adityaShar24 <adityarajansharma1234@gmail.com>
2024-06-07 21:14:58 +0530

    Added UserAuthentication Serializer

---
+++ SocialNetwork/UserAuthentication/serializers.py
@@ -0,0 +1,31 @@
+from .models import User
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def validate_email(self, value):
+        return value.lower()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
